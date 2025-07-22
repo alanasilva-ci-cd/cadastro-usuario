d61e3bc60ac8e799f4c272a4b1bfc069fb440e18
Alana Silva <alanasilva.as@gmail.com>
2025-07-22 18:15:30 -0300

    commit final

--- cadastro-usuario/src/main/java/com/javasilva/cadastro_usuario/business/UsuarioService.java
+++ cadastro-usuario/src/main/java/com/javasilva/cadastro_usuario/business/UsuarioService.java
@@ -17,16 +17,26 @@ public class UsuarioService {
         repository.saveAndFlush(usuario);
     }
 
-    public Usuario buscarUsuarioPorEmail(String email){
+    public Usuario buscarUsuarioPorEmail(String email) {
 
         return repository.findByEmail(email).orElseThrow(
-            () -> new RuntimeException("Email não encontrado")
-        );
+                () -> new RuntimeException("Email não encontrado"));
     }
 
-    public void deletarUsuarioPorEmail(String email){
+    public void deletarUsuarioPorEmail(String email) {
         repository.deleteByEmail(email);
     }
 
-    
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario) {
+        Usuario usuarioEntity = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+
+        repository.saveAndFlush(usuarioAtualizado);
+    }
+
 }
